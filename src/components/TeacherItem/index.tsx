b8686e84d483f379d3d67028d761044ecf12fa35
Cesar Frantz <cesarfrantzsi@gmail.com>
2020-08-04 11:21:59 -0300

    nlw: creating lading page, routes, header, teacher list, adding assets.

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+import "./styles.css";
+
+function TeacherItem() {
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src="https://avatars2.githubusercontent.com/u/8607960?s=460&u=8fff0c168d5bd1fed925d7c5b824f84ba62a4c28&v=4" alt="rasec" />
+                <div>
+                    <strong>Professor 1</strong>
+                    <span>Química</span>
+                </div>
+            </header>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                <br /> <br />
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$ 80,00</strong>
+                </p>
+                <button type="button">
+                    <img src={whatsappIcon} alt="whats" />
+                    Entrar em contato
+                </button>
+            </footer>
+        </article>
+    );
+}
+
+export default TeacherItem;
